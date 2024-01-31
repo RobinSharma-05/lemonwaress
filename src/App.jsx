2e9aa611ec6f5af6cb562d5460f2603169594cdc
RobinSharma-05 <robin9253370222@gmail.com>
2024-01-31 16:17:21 +0530

    names

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import Header from "./components/Header";
+import HeaderSection from "./components/HeaderSection";
 import Section1 from "./components/Section1";
 import Section2 from "./components/Section2";
 import Section3 from "./components/Section3";
 import Section4 from "./components/Section4";
-import Footer from "./components/Footer";
-import Top from "./components/Top";
+import FooterSection from "./components/FooterSection";
+import BackToTop from "./components/BackToTop";
 import Preloader from "./components/Preloader";
 import AOS from "aos";
 import "aos/dist/aos.css";
@@ -19,13 +19,13 @@ function App() {
   return (
     <>
       <Preloader />
-      <Header />
+      <HeaderSection />
       <Section1 />
       <Section2 />
       <Section3 />
       <Section4 />
-      <Footer />
-      <Top />
+      <FooterSection />
+      <BackToTop />
     </>
   );
 }
